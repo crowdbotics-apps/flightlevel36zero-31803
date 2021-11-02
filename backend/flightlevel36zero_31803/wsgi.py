eab29462fb60cfc5d6822c025d117254563f8e36
Crowdbotics <team@crowdbotics.com>
2021-11-02 12:37:31 +0000

    Initial commit.

---
+++ backend/flightlevel36zero_31803/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for flightlevel36zero_31803 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flightlevel36zero_31803.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "flightlevel36zero_31803",
+  url: "https://flightlevel36zero_31803.botics.co",
+  api: "https://flightlevel36zero_31803.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
