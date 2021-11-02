6c81d2e9394440810c1a5561752a452619388c10
Crowdbotics <team@crowdbotics.com>
2021-11-02 12:42:40 +0000

    Update models

--- backend/flightlevel36zero_31803/wsgi.py
+++ backend/flightlevel36zero_31803/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flightlevel36zero_31803.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flightlevel36zero_31803.settings")
 
 application = get_wsgi_application()
